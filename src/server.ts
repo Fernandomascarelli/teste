4b8b629650049a1459a76f2173b5ac16a07f604f
Fernandomascarelli <fernandobiguetti123@gmail.com>
2024-06-25 09:14:23 -0300

    correcao server

--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,43 @@
 import fastify from "fastify";
 import { z } from "zod";
 import fetch from "node-fetch";
-import fs from "fs/promises";
 
 const app = fastify();
 
-app.post('/transform', (request, reply) => {
-    const urlSchema = z.object({
-        url : z.string(),
-    })
+const urlSchema = z.object({
+    url: z.string(),
+});
 
-    const { url } = urlSchema.parse(request.body);
+async function getDataBlob(url: string) {
+    try {
+        const res = await fetch(url);
+        const buffer = await res.buffer(); 
+        const base64Data = buffer.toString("base64");
+        return base64Data;
+    } catch (err) {
+        console.error("Erro ao obter dados:", err);
+        throw err;
+    }
+}
 
-    async function getDataBlob(url: string) {
-        try {
-          const res = await fetch(url);
-          const buffer = await res.buffer(); 
-      
-          const base64Data = buffer.toString("base64");
-      
-          return base64Data;
-        } catch (err) {
-          console.error("Ero:", err);
-          throw err;
-        }
-      }
-      
-      getDataBlob(url)
-        .then((base64Data) => {
-          console.log("Base64:", base64Data);
-        })
-        .catch((err) => {
-          console.error("Ero:", err);
-        });
+app.post('/transform', async (request, reply) => {
+    try {
+        const { url } = urlSchema.parse(request.body);
 
-        return reply.status(200).send(getDataBlob(url))
+        const base64Data = await getDataBlob(url);
 
-})
-
+        return reply.status(200).send(base64Data);
+    } catch (err) {
+        console.error("Erro na requisição:", err);
+        return reply.status(500).send({ error: "Erro ao processar a requisição" });
+    }
+});
 
 app.listen({
     host: '0.0.0.0',
     port: process.env.PORT ? Number(process.env.PORT) : 3147,
 }).then(() => {
-    console.log('HTTP Server Running')
-})
+    console.log('Servidor HTTP rodando');
+}).catch(err => {
+    console.error('Erro ao iniciar o servidor:', err);
+});
